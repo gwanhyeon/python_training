8f35924a50702c4e6251c93fc69718437d592b64
kgh940525 <kgh940525@gmail.com>
2019-05-03 20:14:52 +0900

    [python string]

--- python-relation-operator.py
+++ python-relation-operator.py
@@ -8,8 +8,37 @@ b = 2
 print(a > b)
 
 # 논리 연산자
+
 print((a > b) | (a < b))
 
 # 복합 대입 연산자
 a += b
 print(a)
+
+
+# 비트 연산
+"""
+a&b
+a|b
+a^b
+
+1.
+Multiple Choice
+제곱을 위해 필요한 연산자는? **
+Correct.
+2.
+Multiple Choice
+다음 중 존재하지 않는 복합 대입 연산자는? &&=
+
+*=
+
+&&=
+
+&=
+
+|=
+
++=
+Correct.
+Submit
+"""
---
+++ python-string.py
@@ -0,0 +1,49 @@
+"""
+
+
+print(" hell'o'")
+print(' hell"o"')
+
+1. 이처럼 문자열 내에 큰 따옴표를 포함시키고 싶을 때에는 작은 따옴표를, 작은 따옴표를 포함시키고 싶을 때는 큰 따옴표를 사용하면 됩니다. 
+
+2. 큰 따옴표 사용하고 싶을경우 \(이스케이프 문자)를 사용하면 된다.
+print("\"")
+
+3. 줄바꿈을 하고싶으면 \n
+"""
+
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+# 문자열 더하기
+a = "apple"
+b = "pie"
+c = a+b
+print(c)
+
+# 문자열 곱하기
+
+a = "apple"
+print(a * 2)
+print("hi"*10)
+
+
+"""
+결과
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+# 문자열 더하기
+a = "apple"
+b = "pie"
+c = a+b
+print(c)
+
+# 문자열 곱하기
+
+a = "apple"
+print(a * 2)
+print("hi"*10)
+"""
+
+

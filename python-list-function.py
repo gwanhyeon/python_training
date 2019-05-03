6acb74c0cff09eb25679a267051469bfab0eeab0
kgh940525 <kgh940525@gmail.com>
2019-05-03 21:02:54 +0900

    [python list-function2]

---
+++ python-list-function.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+a = [1, 9, 2, 3, 4, 5, 7]
+
+#a를 오름차순으로 변경 한 뒤 출력
+a.sort()
+print(a)
+
+#a에서 9를 꺼낸 뒤 출력
+a.pop(a.index(2))
+print(a)
+
+#a의 5 뒤에 6을 추가한 뒤 출력
+a.insert(4,6)
+print(a)
+
+#a의 맨 마지막에 8을 추가하고 출력
+a.insert(7,8)
+print(a)
+
+#a를 내림차순 한 뒤 출력
+a.reverse()
+print(a)

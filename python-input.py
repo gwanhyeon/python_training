220f96cf180d97e5970349778865a57615b3cef5
kgh940525 <kgh940525@gmail.com>
2019-05-03 19:53:52 +0900

    [python input,raw-input]

--- python-input.py
+++ python-input.py
@@ -1,4 +1,18 @@
-for i in range(7):
-    print ("Hello ")
-    print(i)
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
 
+# "자신의 나이를 입력해 주세요" 를 age 에 저장
+age = input("My Age is : ")
+# "살고 있는 도시를 입력해 주세요" 를 city 에 저장
+city = input("My City is :") 
+# age와 city 출력
+print(age)
+print(city)
+
+
+"""
+input() 은 입력값에 따라 자료형을 인식하는 반면,
+ raw_input() 은 입력값의 자료형에 상관 없이 입력값을 문자열로 변환시켜 줍니다.
+
+"""
+                

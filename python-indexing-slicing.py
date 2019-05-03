e57afc7a155b19b1b198a45a97136f02d7e34e1c
kgh940525 <kgh940525@gmail.com>
2019-05-03 20:44:30 +0900

    [pyhon list data structure]

--- python-indexing-slicing.py
+++ python-indexing-slicing.py
@@ -6,7 +6,16 @@ a [ 시작번호 : 끝번호 ]와 같이 표현됩니다.
 
 # -*- coding: utf-8 -*-
 # UTF-8 encoding when using korean
-# 문자열 인덱싱,슬라이싱 
+# 문자열 인덱싱,슬라이싱
+"""
+슬라이싱시에 a[4:6] 일경우
+0번 부터 인덱스로 시작해서 4번째의 경우의 값 ~ 6번째를 포함하지 않은 값까지 !
+"""
+a="123456"
+
+print(a[4:6])
+
+
 a = "hello"
 print(a[0]);
 print(a[1]);
@@ -14,7 +23,7 @@ print(a[2]);
 print(a[3]);
 print(a[4]);
 
-a = "I love Coffee"
+a = "I loveCoffee"
 print(a[0:6])
 
 # -*- coding: utf-8 -*-
---
+++ python-list-data-structure.py
@@ -0,0 +1,9 @@
+oddnumber =[1,3,5,7,9]
+cafes = ['a','b','c','d']
+A = [1,5,'A','CC','B']
+listInList = [[1,3,5,8], cafes,oddnumber, 1,3,'Abc']
+
+print(oddnumber)
+print(cafes)
+print(A)
+print(listInList)

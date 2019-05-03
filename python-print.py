71546346837c2575f7c98cab485e0c56f6d5e294
kgh940525 <kgh940525@gmail.com>
2019-05-03 19:46:31 +0900

    [python print,input]

---
+++ python-print.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+# 5 + 5 출력
+var1 = 5;
+var2 = 5;
+print (var1+var2)
+# my_var에 "Hello"를 저장한 뒤 출력
+my_var = "Hello"
+print(my_var)
+
+# "Python" 출력
+print("Python")
